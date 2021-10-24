06078c455b5d216d08507da29db8ffc14a779832
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2021-10-24 18:24:46 -0400

    Move the class creation to a setup method for convenience

--- test_time_service.rb
+++ test_time_service.rb
@@ -3,12 +3,14 @@ require 'minitest/autorun'
 load 'time_service.rb'
 
 class TestTimeService < Minitest::Test
-  def test_the_class_is_initialized_with_a_block
-    t = TimeService::FirstClass::SecondClass.new do |f|
+  def setup
+    @t = TimeService::FirstClass::SecondClass.new do |f|
       f.bar = 123
       f.baz = 456
     end
-    assert_equal t.bar, 123
-    assert_equal t.baz, 456
+  end
+  def test_the_class_is_initialized_with_a_block
+    assert_equal @t.bar, 123
+    assert_equal @t.baz, 456
   end
 end
